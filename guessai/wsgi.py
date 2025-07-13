8b3160176badfb8f2f5fff689fb6ec74d3cc42d1
Max Blackmon <maxblackmon@Maxs-Laptop-2.local>
2025-07-13 16:36:08 -0400

    Restore proper WSGI entrypoint and remove root wsgi.py

--- guessai/wsgi.py
+++ guessai/wsgi.py
@@ -1,7 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-# ← point to guessai.settings, not "settings"!
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "guessai.settings")
 
 application = get_wsgi_application()
